4cab0f5be9d7099f662915dad57d9f62e8d7d74d
Kevin Steve Sathyanath <satke569@student.otago.ac.nz>
2024-04-11 20:56:27 +1200

    Attempting GUI

--- first.py
+++ first.py
@@ -1,10 +1,38 @@
+import tkinter as tk
 from datetime import date
 
-x = int(input('Enter a number\n'))
-print(x)
+def num():
+    try:    
+        x = numFrame.get()
+        y = float(x)
+        result_label.config(text=f"Entered num: {y:.2f}")
+        second_label.config(text = "Thanks")
 
-print(date.today())
+    except ValueError:
+            #Handle invalid input
+            result_label.config(text="Invalid input. PLease enter a valid number.")
+        
+        
+root = tk.Tk()
+root.title("Hello World")
+numFrame = tk.Entry(root)
+numFrame.pack()
 
-a = 'Hello'
-b = 'world'
-print(a,b)
+#Create a button to process input
+sButton = tk.Button(root, text="Submit", command = num)
+sButton.pack()
+
+#Create a label to display the result
+result_label = tk.Label(root, text="")
+result_label.pack()
+
+#Attempting a second label
+second_label = tk.Label(root, text="Trying")
+second_label.pack()
+
+root.mainloop()
+
+
+
+
+

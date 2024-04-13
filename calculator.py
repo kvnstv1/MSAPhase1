d40bb2f97880617d82d445bea5031b0adfa28689
Kevin Steve Sathyanath <satke569@student.otago.ac.nz>
2024-04-13 17:49:32 +1200

    comments

--- calculator.py
+++ calculator.py
@@ -11,7 +11,7 @@ root.title("Calculator")
 entry = tk.Entry(root)
 entry.pack()
 
-#Create buttons
+#Create buttons for the app
 for digit in range(10):
     button = tk.Button(root, text=str(digit), command = lambda d=digit: entry.insert(tk.END, str(d)))
     button.pack(side=tk.LEFT)
